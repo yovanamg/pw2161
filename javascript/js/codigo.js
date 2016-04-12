33f5f561ea57f18307342d5f9f0de4f9e91c3a39
yovanamg <yovanamata94@gmail.com>
2016-04-12 10:59:34 -0600

    Archivo JS, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,8 @@
+var botonClic = function ()
+{
+	alert("Clic del botón");
+}
+	var nombre = "Yovana";
+	    document.write("<strong>JS en acción</strong>"+" "+nombre); 
+	    console.log("apunto de mandar un mjs");
+	    alert("Mensaje por pantalla");
