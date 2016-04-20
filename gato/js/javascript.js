4dc8521133d2cd908b93b321b6b5a89ef03de037
yovanamg <yovanamata94@gmail.com>
2016-04-20 10:20:18 -0600

    cabios gato

--- gato/js/javascript.js
+++ gato/js/javascript.js
@@ -1,25 +1,95 @@
 var bandera = true;
 
-function clic()
-{
-	if(bandera==true)
+function clic(nombre)
+{ 
+	if(document.getElementById("n11").value == "")
 	{
-		document.getElementById("n11").value="x";
-		bandera = false;
+		if(nombre==n11)
+		{
+			if(bandera==true)
+			{document.getElementById("n11").value="X"; bandera = false;}
+			else
+			{document.getElementById("n11").value="O"; bandera = true;}
+		}
 	}
-	else
+	if(document.getElementById("n12").value == "")
 	{
-		document.getElementById("n11").value="o";
+		if(nombre==n12)
+		{
+			if(bandera==true)
+			{document.getElementById("n12").value="X"; bandera = false;}
+			else
+			{document.getElementById("n12").value="O"; bandera = true;}
+		}
 	}
-	if(bandera==true)
+	if(document.getElementById("n13").value == "")
 	{
-		document.getElementById("n12").value="x";
-		bandera = false;
+		if(nombre==n13)
+		{
+			if(bandera==true)
+			{document.getElementById("n13").value="X"; bandera = false;}
+			else
+			{document.getElementById("n13").value="O"; bandera = true;}
+		}
 	}
-	else
+	if(document.getElementById("n21").value == "")
 	{
-		document.getElementById("n12").value="o";
+		if(nombre==n21)
+		{
+			if(bandera==true)
+			{document.getElementById("n21").value="X"; bandera = false;}
+			else
+			{document.getElementById("n21").value="O"; bandera = true;}
+		}
 	}
-
-
+	if(document.getElementById("n22").value == "")
+	{
+		if(nombre==n22)
+		{
+			if(bandera==true)
+			{document.getElementById("n22").value="X"; bandera = false;}
+			else
+			{document.getElementById("n22").value="O"; bandera = true;}
+		}
+	}
+	if(document.getElementById("n23").value == "")
+	{
+		if(nombre==n23)
+		{
+			if(bandera==true)
+			{document.getElementById("n23").value="X"; bandera = false;}
+			else
+			{document.getElementById("n23").value="O"; bandera = true;}
+		}
+	}
+	if(document.getElementById("n31").value == "")
+	{
+		if(nombre==n31)
+		{
+			if(bandera==true)
+			{document.getElementById("n31").value="X"; bandera = false;}
+			else
+			{document.getElementById("n31").value="O"; bandera = true;}
+		}
+	}
+	if(document.getElementById("n32").value == "")
+	{
+		if(nombre==n32)
+		{
+			if(bandera==true)
+			{document.getElementById("n32").value="X"; bandera = false;}
+			else
+			{document.getElementById("n32").value="O"; bandera = true;}
+		}
+	}
+	if(document.getElementById("n33").value == "")
+	{
+		if(nombre==n33)
+		{
+			if(bandera==true)
+			{document.getElementById("n33").value="X"; bandera = false;}
+			else
+			{document.getElementById("n33").value="O"; bandera = true;}
+		}
+	}
 }
