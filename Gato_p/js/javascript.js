2c0ce94a05bd88f56a83c81414a0803e66b57763
yovanamg <yovanamata94@gmail.com>
2016-04-25 11:06:47 -0600

    Gato Paso a Paso

---
+++ Gato_p/js/javascript.js
@@ -0,0 +1,77 @@
+var turno = false;
+var letraCasilla = "";
+var cuantaJuego = 0;
+var cuentaJugadas = 0;
+
+function iniciaGato()
+{
+	//en construccion
+}
+
+function validaJugada(letra)
+{
+	var ganador = false;
+	var b11 = document.getElementById("n11").innerHTML;
+	var b12 = document.getElementById("n12").innerHTML;
+	var b13 = document.getElementById("n13").innerHTML;
+	var b21 = document.getElementById("n21").innerHTML;
+	var b22 = document.getElementById("n22").innerHTML;
+	var b23 = document.getElementById("n23").innerHTML;
+	var b31 = document.getElementById("n31").innerHTML;
+	var b32 = document.getElementById("n32").innerHTML;
+	var b33 = document.getElementById("n33").innerHTML;
+	//jugadas
+	//renglones
+	if(b11 == b12 && b12 == b13 && b11! = "&nbsp;")
+	ganador = true;
+	if(b21 == b22 && b22 == b23 && b21! = "&nbsp;")
+	ganador = true;
+	if(b31 == b33 && b32 == b33 && b31! = "&nbsp;")
+	ganador = true;
+	if(b11 == b12 && b12 == b13 && b11! = "&nbsp;")
+	ganador = true;
+	//columnas
+	if(b11 == b21 && b21 == b31 && b11! = "&nbsp;")
+	ganador = true;
+	if(b12 == b22 && b22 == b32 && b12! = "&nbsp;")
+	ganador = true;
+	if(b13 == b23 && b23 == b33 && b13! = "&nbsp;")
+	ganador = true;
+	//diagonales
+	if(b11 == b22 && b22 == b23 && b11! = "&nbsp;")
+	ganador = true;
+	if(b13 == b22 && b22 == b31 && b13! = "&nbsp;")
+	ganador = true;
+	//quien gano
+	if (ganador == true) //if(ganador)
+	{
+		alert("¡ganador!" + letra + "!");
+	}
+	else if (ganador == false && cuentaJugadas == 9)
+	{
+		alert("¡Empate!")
+	}
+}
+
+function escribe(casilla)
+{
+	var letra = "";
+	letraCasilla = document.getElementById(casilla).innerHTML;
+	//if(letraCasilla == "&nbsp")
+	if (letraCasilla !="X" && letraCasilla ="O")
+	{
+		if(turno == false) //if(!turno)
+		{
+			letra = "X";
+		}
+		else
+		{
+			letra = "O";
+			document.getElementById(casilla).innerHTML = letra;
+			turno = !turno; //turno = false
+		}
+	}
+	cuentaJugadas = cuentaJugadas + 1;
+	//para saber quien gano validamos la jugada
+	validaJugada(letra);
+}
